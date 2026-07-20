471a6b59b9270697cd02fc668fd67a2f2f4190a7
Kumar Dhamala <dhamalakumar834@gmai.com>
2026-07-20 14:13:08 +0545

    Updated build sh

--- build.sh
+++ build.sh
@@ -9,3 +9,12 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email='garment@admin.com').exists():
+    User.objects.create_superuser(email='garment@admin.com', password='garment@Admin')
+    print('Superuser created successfully!')
+"
