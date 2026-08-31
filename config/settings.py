b138fed829c51056fcd047f3161e779cbf72bd06
Kumar Dhamala <dhamalakumar834@gmai.com>
2026-08-31 11:52:05 +0545

    Updated Settings

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,10 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config, Csv
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
